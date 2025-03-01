cf2d69430952b50a43ad8123e6e8c3361337b54f
Shellbaka <shellbaka69@gmail.com>
2025-03-01 18:58:43 -0300

    Initial commit

---
+++ src/test/java/DIO/Santander/Spring/project/SantanderSpringProjectMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package DIO.Santander.Spring.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderSpringProjectMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
